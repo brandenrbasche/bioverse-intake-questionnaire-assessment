3d8c475d62fe3207f5dda78d2b00c34cc3dcb89f
Branden Basche <brandenbasche@gmail.com>
2024-08-17 17:33:12 -0400

    removed unnecessary comments

--- client/src/app/api/route.ts
+++ client/src/app/api/route.ts
@@ -1,10 +1,8 @@
 import { getServerSession} from "next-auth";
 import { NextResponse } from "next/server";
-// import { authOptions} from "@/app/api/auth/[...nextauth]/route";
 import { authOptions } from "../../../lib/auth";
 
 export async function GET(request: Request) {
     const session = await getServerSession(authOptions);
-    console.log('GET API', session);
     return NextResponse.json({ authenticated: !!session })
 }
