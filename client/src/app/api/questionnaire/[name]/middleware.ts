5989a71746b0655ef440eb0e5f4259292b37c41b
Branden Basche <brandenbasche@gmail.com>
2024-08-17 18:14:21 -0400

    resolving build errors

--- client/src/app/api/questionnaire/[name]/middleware.ts
+++ client/src/app/api/questionnaire/[name]/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import {prisma} from "../../../../../lib/prisma";
 
-export async function getQuestionnaireIdByName(request: NextRequest, questionnaireName: string) {
+export async function getQuestionnaireIdByName(request: NextRequest, questionnaireName: string | undefined) {
     try {
         const result = await prisma.questionnaire_questionnaires.findFirst({
             where: {
